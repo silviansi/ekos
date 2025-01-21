3e7dacb4386a9976e227afa94695e2c79134b70f
silviansi <silviansi24@gmail.com>
2025-01-21 11:50:40 +0700

    styling login with bootstrap

--- index.php
+++ index.php
@@ -31,18 +31,32 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login eKos</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h2>Login eKos</h2>
-    <?php if (isset($error)): ?>
-        <p style="color: red"><?= $error ?></p>
-    <?php endif; ?>
-    
-    <form action="index.php" method="POST">
-        <label for="username">Username</label><br>
-        <input type="text" name="username" id="username"><br><br>
-        <label for="password">Password</label><br>
-        <input type="password" name="password" id="password"><br><br>
-        <button type="submit">Login</button>
+<div class="container vh-100 d-flex justify-content-center align-items-center">
+        <div class="bg-light p-5 rounded shadow-lg text-center" style="width: 100%; max-width: 400px;">
+            <h2 class="mb-4">Login eKos</h2>
+            <?php if (isset($error)): ?>
+                <p class="text-danger"><?= $error ?></p>
+            <?php endif; ?>
+
+            <form action="index.php" method="POST">
+                <div class="mb-3">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" name="username" id="username" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name="password" id="password" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-primary w-100">Login</button>
+            </form>
+        </div>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
