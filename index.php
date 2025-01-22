47895f2c1e947eaebe9ee206acaabea9e00240a1
silviansi <silviansi24@gmail.com>
2025-01-22 20:34:13 +0700

    add background image

--- index.php
+++ index.php
@@ -33,26 +33,30 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Login eKos</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-<div class="container vh-100 d-flex justify-content-center align-items-center">
-        <div class="bg-light p-5 rounded shadow-lg text-center" style="width: 100%; max-width: 400px;">
-            <h2 class="mb-4">Login eKos</h2>
-            <?php if (isset($error)): ?>
-                <p class="text-danger"><?= $error ?></p>
-            <?php endif; ?>
+    <div class="login">
+        <div class="container vh-100 d-flex justify-content-center align-items-center">
+            <div class="bg-light p-5 rounded shadow-lg text-center" style="width: 100%; max-width: 400px;">
+                <h2 class="mb-4">Login eKos</h2>
+                <?php if (isset($error)): ?>
+                    <p class="text-danger"><?= $error ?></p>
+                <?php endif; ?>
 
-            <form action="index.php" method="POST">
-                <div class="mb-3">
-                    <label for="username" class="form-label">Username</label>
-                    <input type="text" name="username" id="username" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" name="password" id="password" class="form-control">
-                </div>
-                <button type="submit" class="btn btn-primary w-100">Login</button>
-            </form>
+                <form action="index.php" method="POST">
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" name="username" id="username" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" name="password" id="password" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-primary w-100">Login</button>
+                </form>
+            </div>
         </div>
     </div>
 
