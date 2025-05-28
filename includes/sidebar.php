2c8f97cc6fb0aca9763090f04cab0cc1df8336a8
silviansi <silviansi24@gmail.com>
2025-05-28 20:29:49 +0700

    add elements for dashboard

---
+++ includes/sidebar.php
@@ -0,0 +1,84 @@
+<nav class="pc-sidebar">
+  <div class="navbar-wrapper">
+    <div class="m-header">
+      <a href="/ekos/dashboard.php" class="b-brand text-primary">
+        <!-- ========   Change your logo from here   ============ -->
+        <img src="/ekos/assets/img/logo-ekos.png" class="img-fluid w-50" alt="logo">
+      </a>
+    </div>
+    <div class="navbar-content">
+      <ul class="pc-navbar">
+        <li class="pc-item">
+          <a href="/ekos/dashboard.php" class="pc-link">
+            <span class="pc-micon"><i class="ti ti-dashboard"></i></span>
+            <span class="pc-mtext">Dashboard</span>
+          </a>
+        </li>
+
+        <li class="pc-item pc-caption">
+          <label>Manajemen Pengguna</label>
+          <i class="ti ti-dashboard"></i>
+        </li>
+        <li class="pc-item">
+          <a href="/ekos/pages/admin/manage-users.php" class="pc-link">
+            <span class="pc-micon"><i class="ti ti-shield"></i></span>
+            <span class="pc-mtext">Kelola Pengguna</span>
+          </a>
+        </li>
+        <li class="pc-item">
+          <a href="/ekos/pages/admin/tenants-data.php" class="pc-link">
+            <span class="pc-micon"><i class="ti ti-user"></i></span>
+            <span class="pc-mtext">Data Penyewa</span>
+          </a>
+        </li>
+        <li class="pc-item">
+          <a href="/ekos/pages/admin/owners-data.php" class="pc-link">
+            <span class="pc-micon"><i class="ti ti-users"></i></span>
+            <span class="pc-mtext">Data Pemilik Kos</span>
+          </a>
+        </li>
+
+        <li class="pc-item pc-caption">
+          <label>Manajemen Kos</label>
+          <i class="ti ti-brand-chrome"></i>
+        </li>
+        <li class="pc-item">
+          <a href="elements/icon-tabler.html" class="pc-link">
+            <span class="pc-micon"><i class="ti ti-building"></i></span>
+            <span class="pc-mtext">Data Kos</span>
+          </a>
+        </li>
+        <li class="pc-item">
+          <a href="elements/icon-tabler.html" class="pc-link">
+            <span class="pc-micon"><i class="ti ti-shield-check"></i></span>
+            <span class="pc-mtext">Verifikasi Kos</span>
+          </a>
+        </li>
+        <li class="pc-item">
+          <a href="elements/icon-tabler.html" class="pc-link">
+            <span class="pc-micon"><i class="ti ti-archive"></i></span>
+            <span class="pc-mtext">Kategori Kos</span>
+          </a>
+        </li>
+        <li class="pc-item">
+          <a href="elements/icon-tabler.html" class="pc-link">
+            <span class="pc-micon"><i class="ti ti-tool"></i></span>
+            <span class="pc-mtext">Fasilitas Kos</span>
+          </a>
+        </li>
+
+        <li class="pc-item pc-caption">
+          <label>Others</label>
+          <i class="ti ti-brand-chrome"></i>
+        </li>
+        <li class="pc-item">
+          <a href="elements/icon-tabler.html" class="pc-link">
+            <span class="pc-micon"><i class="ti ti-message"></i></span>
+            <span class="pc-mtext">Pesan Masuk</span>
+          </a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
