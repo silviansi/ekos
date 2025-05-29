e5a9f47f2de730e7eb939a7f45af298042b2c98a
silviansi <silviansi24@gmail.com>
2025-05-29 11:34:46 +0700

    update redirect page based on user role

--- login.php
+++ login.php
@@ -27,10 +27,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // Redirect sesuai dengan role_id
             switch ($user['role_id']) {
                 case 1:
-                    header('Location: pages/admin/dashboard.php');
+                    header('Location: /ekos/pages/admin/dashboard.php');
                     break;
                 case 2:
-                    header('Location: pages/pemilik/dashboard.php');
+                    header('Location: /ekos/pages/pemilik/dashboard.php');
                     break;
                 case 3:
                     header('Location: index.php');
@@ -53,11 +53,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>eKos</title>
+    <title>eKos - Masuk</title>
+    <!-- [Favicon] icon -->
+    <link rel="icon" href="assets/img/logo.png" type="image/x-icon"> 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
     <main class="main-login d-flex justify-content-center align-items-center">
