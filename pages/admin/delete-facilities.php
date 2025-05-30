36fce673a96430f84659c1399f76771f12f92de3
silviansi <silviansi24@gmail.com>
2025-05-30 19:11:34 +0700

    feat: functional delete facilities kost

---
+++ pages/admin/delete-facilities.php
@@ -0,0 +1,26 @@
+<?php 
+require '../../config/database.php';
+
+// Cek apakah ada ID fasilitas yang dihapus
+if (isset($_GET['id'])) {
+    $facility_id = $_GET['id'];
+
+    // Siapkan query untuk menghapus fasilitas
+    $stmt = $conn->prepare("DELETE FROM facilities WHERE facility_id = :id");
+    $stmt->bindParam(':id', $facility_id, PDO::PARAM_INT);
+
+    if ($stmt->execute()) {
+        // Redirect ke halaman data fasilitas dengan pesan sukses
+        header("Location: /ekos/pages/admin/facility-kost.php?success=delete");
+        exit();
+    } else {
+        // Redirect ke halaman data fasilitas dengan pesan error
+        header("Location: /ekos/pages/admin/facility-kost.php?error=delete");
+        exit();
+    }
+} else {
+    // Jika tidak ada ID yang diberikan, redirect ke halaman data fasilitas
+    header("Location: /ekos/pages/admin/facility-kost.php?error=invalid");
+    exit();
+}
+?>
